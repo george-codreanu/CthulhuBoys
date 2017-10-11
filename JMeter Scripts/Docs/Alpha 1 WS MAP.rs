f70d63e1fcc01d0803ef2f92c18c8f1c47a4b038
geaniFleancu <geani.fleancu@tremend.com>
2017-10-11 14:02:57 +0300

    added trransaction uload oducment ... NOT WORKING FOR TEST ENV !!!

--- JMeter Scripts/Docs/Alpha 1 WS MAP.rs
+++ JMeter Scripts/Docs/Alpha 1 WS MAP.rs
@@ -17,3 +17,30 @@ GET http://ro-test.rbro.rbg.cc/services/fx-rates-ws/v1/buy/RON/JPY
 [INTEREST RATES]
 
 GET http://ro-test.rbro.rbg.cc/services/interest-rates/v1/interest-rates
+
+[LOGIN]
+
+POST http://ro-test.rbro.rbg.cc/services/auth/v1/login
+
+POST data:
+{"password":"test1234","username":"vn-84-vly"}
+
+[DASHBOARD]
+
+GET http://ro-test.rbro.rbg.cc/services/marketing-service/v1/isEligible
+
+GET http://ro-test.rbro.rbg.cc/services/accounts-ws/v1/accounts/listHolds
+
+GET http://ro-test.rbro.rbg.cc/services/accounts-ws/v1/accounts/list
+
+[CARDS]
+
+GET http://ro-test.rbro.rbg.cc/services/cards-ws/v2/cards?page=0&size=10&status=All&clientType=PF&sourceApplication=IBK
+
+[ACCOUNT OVERVIEW]
+
+POST http://ro-test.rbro.rbg.cc/services/transactions-service/v1/transactions
+{"accountId":"0005161502","accountType":"20","size":10,"transactionStatusTypes":["SUCCESS"]}
+
+POST http://ro-test.rbro.rbg.cc/services/transactions-service/v1/transactions
+{"accountId":"0005161502","accountType":"20","size":100,"transactionStatusTypes":["HOLD"]}
