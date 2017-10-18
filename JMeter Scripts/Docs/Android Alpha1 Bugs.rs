f8dcad5de00d51990f9208d77436934f327bf4b6
geaniFleancu <geani.fleancu@tremend.com>
2017-10-18 16:32:05 +0300

    mcaffe script stress test v1 ... 15 upl trans ... small scrips adjkustments k

---
+++ JMeter Scripts/Docs/Android Alpha1 Bugs.rs
@@ -0,0 +1,14 @@
+ARO-4109 - accountType99 block
+
+[BUGS]
+//locator crash .. open move and open again - ARO-4301
+
+Contact_07,10,13,14 - diacritice for social label
+ExcRate_04 - Step 1 - diacritite 
+ExcRate_06 - Step 3 - diacritice
+ExcRate_08 - step 4,7 - diacritice
+//ExcRate_08 - step ?? - type number with decimals .. switch ... try to type again after decimal ..press ok from keyboard..switch ...app crash - ARO-4294
+//ExcRate_08 - step ?? - select RO ... press , press . press . -> crash  - ARO-4293
+Loc_18 - step 1 - alpha numeric chararacters can be added on search- ARO-4354
+ARO-4351 
+ARO-4355
